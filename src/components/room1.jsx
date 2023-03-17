3bf4a22b2afe5acd15e83b9c0ac475d2beb7cce1
Aidan <iiiaiid4niii@gmail.com>
2023-03-17 22:07:40 +0000

    working on having position of dots fixed to image independant of screen size / movement

--- src/components/room1.jsx
+++ src/components/room1.jsx
@@ -1,29 +1,39 @@
-import '../App.css';
-import { useState } from "react"
-
+import "../App.css";
+import { useState } from "react";
 
 const Room1 = () => {
-    const [elementCoords, setElementCoords] =useState([])
+  const [elementCoords, setElementCoords] = useState([]);
 
-    const handleClick = (event) => {
-        
-        
-        let targetRect = event.currentTarget.getBoundingClientRect()
-        var xPosition = event.clientX - targetRect.left - 6.25
-        var yPosition = event.clientY - targetRect.top - 6.25
+  const handleClick = (event) => {
+    console.log(event.currentTarget.getBoundingClientRect())
+    let targetRect = event.currentTarget.getBoundingClientRect();
+    var xPosition = event.clientX  - 6.25;
+    var yPosition = event.clientY  - 6.25;
 
+    setElementCoords((currElementCoords) => [
+      ...currElementCoords,
+      [xPosition, yPosition],
+    ]);
+  };
+  return (
+    <div className="room1page_container">
+      <div className="room1_img_div" onClick={handleClick}>
+        {elementCoords.map((coords) => {
+          return (
+            <button
+              className="click_buttons"
+              style={{
+                position: "fixed",
+                left: coords[0],
+                top: coords[1],
+                border: "2px solid black",
+              }}
+            ></button>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-        setElementCoords((currElementCoords) => [...currElementCoords, [xPosition, yPosition]])
-        
-    }
-    return (
-        <div className="room1_div" onClick={handleClick}>
-            {elementCoords.map((coords) => {
-                return <button  className='click_buttons' style={{position:  "fixed",left: coords[0], top: coords[1], border: "2px solid black"}}></button>
-            })}
-        </div>
-        
-    )
-}
-
-export default Room1
+export default Room1;
