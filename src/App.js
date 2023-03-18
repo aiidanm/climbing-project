4e6e556b2d0950a530c677c2c453519db24738b5
Aidan <iiiaiid4niii@gmail.com>
2023-03-18 21:10:37 +0000

    removed link for deploying

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Room1 from './components/room1';
 import Header from './components/header';
-import {Routes, Route, Link}  from "react-router-dom"
+import {Routes, Route}  from "react-router-dom"
 import NavButtons from './components/navbuttons';
 
 function App() {
