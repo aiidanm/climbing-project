7c8d2fa99fa88a82400b63fa048bdfb240c14d23
Aidan <iiiaiid4niii@gmail.com>
2023-03-18 21:08:37 +0000

    finally the buttons stay in place after placement even if screensize changes

--- src/components/room1.jsx
+++ src/components/room1.jsx
@@ -1,19 +1,31 @@
 import "../App.css";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Room1 = () => {
   const [elementCoords, setElementCoords] = useState([]);
+  const [x, setX] = useState();
+  const [y, setY] = useState();
+
+  const myRef = useRef();
+
+  const getPosition = () => {
+    const x = myRef.current.offsetLeft;
+    setX(x);
+    const y = myRef.current.offsetTop;
+    setY(y);
+  };
+
+  useEffect(() => {
+    getPosition();
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("resize", getPosition);
+  }, []);
 
   const handleClick = (event) => {
-    let targetRect = event.currentTarget.getBoundingClientRect();
-
-    var xPosition = event.clientX  - 6.25;
-    var yPosition = event.clientY  - 6.25;
-     let imgXPos = targetRect.x 
-    let imgYPos = targetRect.y
-
-    console.log(imgXPos, imgYPos, "bounding rect")
-    console.log(xPosition, yPosition, "event clients")
+    var xPosition = event.clientX - x - 6.25;
+    var yPosition = event.clientY - y - 6.25;
 
     setElementCoords((currElementCoords) => [
       ...currElementCoords,
@@ -22,15 +34,15 @@ const Room1 = () => {
   };
   return (
     <div className="room1page_container">
-      <div className="room1_img_div" onClick={handleClick}>
+      <div className="room1_img_div" onClick={handleClick} ref={myRef}>
         {elementCoords.map((coords) => {
           return (
             <button
               className="click_buttons"
               style={{
                 position: "fixed",
-                left: coords[0],
-                top: coords[1],
+                left: coords[0] + x,
+                top: coords[1] + y,
                 border: "2px solid black",
               }}
             ></button>
