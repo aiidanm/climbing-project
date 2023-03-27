e2f16962ee62b743de52b6a55de8a693e7b2194e
Aidan <iiiaiid4niii@gmail.com>
2023-03-27 07:38:16 +0100

    added alt text

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,11 @@
-
-
 const Header = () => {
   return (
     <>
-      <img src="Cruxlogo.png" id="logo"></img>
+      <img
+        src="Cruxlogo.png"
+        id="logo"
+        alt="image of the word Crux but the c is a carabiner"
+      ></img>
       <p className="hiremelink">
         <a href="https://www.linkedin.com/in/aidanalexandermurray/">
           hire me pls
