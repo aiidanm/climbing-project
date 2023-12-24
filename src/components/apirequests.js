5c462353b3ee1d1d94d1854c30d7090e9f125dcd
Aidan <iiiaiid4niii@gmail.com>
2023-12-24 18:54:56 +0000

    added .env file, need to test

--- src/components/apirequests.js
+++ src/components/apirequests.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
+
 exports.getRoom1Climbs = () => {
   return fetch("https://climbingdatabasefirebase.onrender.com/climbs/all").then(
-    (data) => data.json().then((climbs) => {
-      const room1 = climbs.filter((climb) => climb.room === 1)
-      return room1
-    })
+    (data) =>
+      data.json().then((climbs) => {
+        const room1 = climbs.filter((climb) => climb.room === 1);
+        return room1;
+      })
   );
 };
 
@@ -14,50 +17,53 @@ exports.postNewClimb = (climb) => {
     headers: {
       "Content-type": "application/json; charset=UTF-8",
     },
-  })
-    .catch((err) => console.log("error on post", err));
+  }).catch((err) => console.log("error on post", err));
 };
 
 exports.getRoom2Climbs = () => {
   return fetch("https://climbingdatabasefirebase.onrender.com/climbs/all").then(
-    (data) => data.json().then((climbs) => {
-      const room2 = climbs.filter((climb) => climb.room === 2)
-      return room2
-    })
+    (data) =>
+      data.json().then((climbs) => {
+        const room2 = climbs.filter((climb) => climb.room === 2);
+        return room2;
+      })
   );
 };
 exports.getRoom3Climbs = () => {
   return fetch("https://climbingdatabasefirebase.onrender.com/climbs/all").then(
-    (data) => data.json().then((climbs) => {
-      const room3 = climbs.filter((climb) => climb.room === 3)
-      return room3
-    })
+    (data) =>
+      data.json().then((climbs) => {
+        const room3 = climbs.filter((climb) => climb.room === 3);
+        return room3;
+      })
   );
 };
 
 exports.postNewUser = (user) => {
-  return fetch ("https://climbingdatabasefirebase.onrender.com/api/signup", {
+  return fetch("https://climbingdatabasefirebase.onrender.com/api/signup", {
     method: "post",
     body: JSON.stringify(user),
     headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    }
-  }).then((res) => res.json().then((parsedRes) => {
-    if(parsedRes.error) {
-      return parsedRes.error
-    } else {
-      return parsedRes.user
-    }
-    
-  }))
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  }).then((res) =>
+    res.json().then((parsedRes) => {
+      if (parsedRes.error) {
+        return parsedRes.error;
+      } else {
+        return parsedRes.user;
+      }
+    })
+  );
 };
 
 exports.login = (user) => {
+
   return fetch("https://climbingdatabasefirebase.onrender.com/api/signin", {
     method: "post",
     body: JSON.stringify(user),
     headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    }
-  }).then((user) => user.json().then((parsedUser) => console.log(parsedUser)))
-}
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  }).then((user) => user.json().then((parsedUser) => console.log(parsedUser)));
+};
